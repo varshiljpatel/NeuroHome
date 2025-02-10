ad3639e62554727b48db0cfa78fa8415c8a928bd
varshiljpatel <mail.varshil@gmail.com>
2025-02-10 23:50:40 +0530

    init:scripts

--- scripts/license.ts
+++ scripts/license.ts
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2025 Varshil J. Patel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import fs from "fs";
 const { join } = require("path");
 const { globSync } = require("glob");
@@ -7,7 +24,7 @@ const licenseHeader = fs.readFileSync(
     "utf-8"
 );
 
-const copyrightPattern = /Copyright \d{4} ascen/;
+const copyrightPattern = /Copyright \d{4} Varshil J. Patel/;
 const globPattern = "+(scripts|apps)/**/*.+(ts|mjs|cjs|js|tsx|jsx)";
 
 async function readFiles(paths: string[]) {
